844c70d61e2f888e025f2ac1b716a20c6de60372
Sandile <sgcinohsithole@gmail.com>
2021-01-08 15:55:54 +0200

    Added a repo which contains a react app with answer to the last question

---
+++ web-page/src/App.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {useForm} from "react-hook-form";
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const {register, handleSubmit,errors}=useForm();
+
+  const onSubmit=(values)=>console.log(values);
+  return(
+     <div className="App">
+       <form onSubmit={handleSubmit(onSubmit)}>
+        
+         <label>
+           
+            Name
+           <input type="text" name="name" ref={register({required:true,maxLength:30,
+            pattern:{
+              value:/^[A-Z]+$/i,
+              message: "Alphabets Only",
+            },
+           })} />
+           
+         </label>
+         <br></br>
+         <label>
+           
+           Surname
+           <input type="text" name="surname" ref={register({required:true,maxLength:30,
+            pattern:{
+              value:/^[A-Z]+$/i,
+              message: "Alphabets Only",
+            },
+           })} />
+           
+         </label>
+         <br></br>
+         <label>
+           
+           Email
+           <input type="email" name="email" ref={register({
+             required:"Email is required",
+             pattern:{
+               value:{value:/^[A-Z0-9._%+-]+@[A-Z0-9-]+\.[A-Z]{2,4}$/i, message:"Invalid email"},
+               message: "Invalid email address.",
+
+             },
+          })} />
+           
+         </label>
+         {errors.email && <span>{errors.email.message}</span>}
+         <br></br>
+         <label>
+           
+           Phone
+           <input type="number" name="phone" ref={register({required:true,minLength:9,maxLength:11})}/>
+           
+         </label>
+         <br></br>
+         <button type="submit">Submit</button>
+         <br></br>
+       </form>
+     </div>
+  );
+}
+export default App;
