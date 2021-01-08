3603610d22329857ddb3f27aa6a7f5672634816f
Sandile <sgcinohsithole@gmail.com>
2021-01-08 10:15:25 +0200

    Added a solution for 2nd problem and named Quuestion2.js

---
+++ Question2.js
@@ -0,0 +1,11 @@
+/*function cubes(a) {
+    retunr a ** 3
+    }
+    */
+   //correct the above function as below
+   function cubes(a){
+    return a**3;
+}
+
+//test to see if the function works
+console.log(cubes(3));
