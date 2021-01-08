2b09a6980a2b7414fd72213f69ebb9a2ce86b2e5
Sandile <sgcinohsithole@gmail.com>
2021-01-08 10:09:00 +0200

    Added a solution named Question1.js

---
+++ Question1.js
@@ -0,0 +1,8 @@
+// function to calculate area of a triangle
+function areaTriangle(base,height){
+    return base*height;
+ }
+ //test cases to see if the function works
+ console.log(areaTriangle(3,4)); //test for integer parameters
+ console.log(areaTriangle(2.5,3.5));//test for decimal or double parameters
+ 
