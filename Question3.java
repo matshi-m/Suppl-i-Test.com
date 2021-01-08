237b5d955051a8e27910de916bf3dffd64a25393
Matshi Matolo <65823211+matshi-m@users.noreply.github.com>
2021-01-08 16:51:33 +0200

    Update Question3.java
    added comments on quality of code.

--- Question3.java
+++ Question3.java
@@ -4,14 +4,14 @@ import java.io.*;
 public class Question3{
     // A function to determine if 1st string ends with a 2nd string
     public boolean endsWithSecond(String first,String second){
-        if(first.endsWith(second))
+        if(first.endsWith(second)) // very clever to just use the Java API for this. welldone !
             return true;
         else 
             return false;
     }
     public static void main(String[] args)
     {
-        Question3 q3=new Question3();
+        Question3 q3=new Question3(); // thank you for added the test cases to validate that all is in working condition.
         // test cases for the string function written above
         System.out.println(q3.endsWithSecond("abx","ihd"));
         System.out.println(q3.endsWithSecond("nbv","v"));
